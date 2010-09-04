1ea23014b81caa4077e125cdaef73d22a4b12937
Shaddi Hasan <shaddih@gmail.com>
2010-09-04 13:38:28 -0700

    Added some usage comments, made interface lists seperate arrays.

--- pomodoro.py
+++ pomodoro.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python
+#
+# pomodoro.py: a little script to help your productivity.
+# Shaddi Hasan, 2010. http://sha.ddih.org.
+#
+# pomodoro.py is a simple script to help increase your focus and productivity.
+# After you run the script, you'll have ten minutes of uninterrupted work time,
+# which is enforced by disabling your network adapters. When ten minutes is up,
+# you've earned a two minute break, and your network adapters will be brought
+# back up. 
+#
+# To run this, make sure you have the required imports on your system, then 
+# modify the interfaces lists below to reflect the names of your system's 
+# network adapters. Then run:
+#
+#          $ sudo pomodoro &
+# 
+# If you need to kill it, open up another terminal and kill the process (but 
+# resist the temptation!). 
 
 import pynotify
 import os
@@ -12,23 +30,28 @@ rest = pynotify.Notification("Break!","Good job! You earned a break!")
 rest.set_timeout(3)
 
 # Timings
-sec_to_run = 3*60*60
-work_time = 10*60
-rest_time = 2*60
+sec_to_run = 3*60*60 # 3 hour total runtime
+work_time = 10*60 # 10 minutes of work time
+rest_time = 2*60 # 2 minutes of break time
 
 # Other settings
-disable_network = True
-disable_interrupts = True
+disable_network = True # Set to false if the internet isn't a distraction to you (?!)
+disable_interrupts = True # Makes it a little harder to kill (but not much)
 
+# modify the interfaces list to match your particular system...
 def turn_off_internet():
-	os.system("ifconfig eth0 down")
-	os.system("ifconfig wlan0 down")
+	interfaces = ["eth0","wlan0"]
+	for interface in interfaces:
+		os.system("ifconfig " + interface + " down")
 
 def turn_on_internet():
-	os.system("ifconfig eth0 up")
+	interfaces = ["eth0","wlan0"]
+	for interface in interfaces:
+		os.system("ifconfig " + interface + " up")
 
 end_time = int(time.time()) + sec_to_run
 
+# Main loop
 while int(time.time()) < end_time:
 	try:
 		# work for a bit
